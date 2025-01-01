581813f3995f9844c730da3bcb7d4b9a6997c361
pasted0 <pastedv1ontop@gmail.com>
2025-01-01 14:45:48 -0600

    Update ESP.lua

--- ESP.lua
+++ ESP.lua
@@ -1,4 +1,12 @@
 
+local function notification(Title,Text,Duration)
+game:GetService("StarterGui"):SetCore("SendNotification", {
+    Title = Title,
+    Text = Text,
+    Duration = Duration
+})
+end
+notification("Credits:", "Made by @Pasted0 on Discord (CLD Devs On Top)", 5)
 while task.wait() do
     for _,v in game:FindService("Players"):GetPlayers() do
       if v.Character and v.Name ~= game.Players.LocalPlayer.Name then 
